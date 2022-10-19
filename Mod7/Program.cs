74ad41eed9f47dfdc74843b8f05a29f71b411f97
ShamilHajiyev <shamilnh@code.edu.az>
2022-10-19 18:21:51 +0400

    initial commit

---
+++ Mod7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mod7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int mod = input % 7;
+            int result;
+            if (mod > 3)
+            {
+                result = input + 7 - mod;
+                Console.WriteLine("\nNearest number:\n" + result);
+            }
+            else if (mod == 0)
+            {
+                Console.WriteLine("\nEntered number is divisible by seven");
+            }
+            else
+            {
+                result = input - mod;
+                Console.WriteLine("\nNearest number:\n" + result);
+            }
+        }
+    }
+}
